23ae4c7c4b128aabcdf77a344220c14b241dd81a
Jose <jimarb@outlook.com>
2019-07-26 21:01:13 -0400

    Desafio 1 terminado

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'users#new'
+  get 'users/sign_up', to: "users#new"
+  post 'users', to: "users#create"
+  get 'users/sign_in', to: "sessions#new"
+  resources :sessions, only: [:create, :destroy]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
